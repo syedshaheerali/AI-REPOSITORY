b60b88a1d30a40630a91c79017d90f821002e12b
Ezharkarim <ezharkarim19@gmail.com>
2022-05-17 14:11:04 +0500

    Add files via upload

---
+++ Assignment3/Ezhar karim KNN.py
@@ -0,0 +1,34 @@
+import pandas as pd # For data cleaning
+import numpy as np # Numerical Methods
+from sklearn.metrics import accuracy_score # For Checking Accuracy
+from sklearn.model_selection import train_test_split # Splitting Data For Train Test
+from sklearn import tree # ML Algo Decision Tree
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier # ML Algo KNN
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+df = pd.read_csv('/content/trainDF.csv')
+df.head()
+
+# Separating Target & Other Columns 
+XTT = df.drop(columns=['f_00'])
+yTT = df['f_00']  
+X_train, X_test, y_train, y_test = train_test_split(XTT, yTT, test_size=0.2)
+
+
+
+# Decision Tree
+modelDT = tree.DecisionTreeClassifier();
+resultDT = modelDT.fit(X_train, y_train);
+prediction_test = modelDT.predict(X_test)
+accuracyDT = metrics.accuracy_score(y_test, prediction_test)
+print()
+print ("Model Accuracy (Decision Tree):" "\n", accuracyDT)
+
+modelKNN = KNeighborsClassifier(n_neighbors=5)
+resultKNN = modelKNN.fit(X_train, y_train)
+prediction_test = modelKNN.predict(X_test)
+accuracyKNN = metrics.accuracy_score(y_test, prediction_test)
+print("Model Accuracy (KNN):" "\n", accuracyKNN)
